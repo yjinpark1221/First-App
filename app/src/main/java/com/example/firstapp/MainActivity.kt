ebca13ab772115bd1c94bcb0c3ac7346b2e7f2d8
bms <ms021230@kaist.ac.kr>
2023-07-04 21:32:36 +0900

    mineSweeperTimerUpdate

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity(), BoardAdapter.OnItemClickListener {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_phonebook, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_phonebook, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_albumDetailsFragment
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/example/firstapp/ui/dashboard/AlbumDetailsFragment.kt
+++ app/src/main/java/com/example/firstapp/ui/dashboard/AlbumDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
